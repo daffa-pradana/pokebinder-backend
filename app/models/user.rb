f8f5b2ccb92c24914150b97463d18f26f28d4da3
Daffa Pradana <daffaarravi@gmail.com>
2025-12-04 22:00:26 +0700

    define refresh_tokens in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true
   validates :password, length: { minimum: 6 }, if: -> { password.present? }
+
+  has_many :refresh_tokens, dependent: :destroy
 end
